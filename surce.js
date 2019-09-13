a27cba2f226bc236bc261709c025703d12019871
Nicholas Upshaw <nicholas.upshaw@yahoo.com>
2019-09-12 20:44:53 -0400

    start of my practice

---
+++ surce.js
@@ -0,0 +1,21 @@
+//Start at the word bank
+var movies = ["Black Panther", "John Wick","The Lion King","Bad Boys","House Party","It","Us",
+"Candyman","Final Destination"]
+
+//Game variables
+var wins = 0;
+var guesses = 10;
+var guessesLeft = [];
+var lettersGuessed = [];
+var matchedLetters = [];
+
+//Functions portion.
+
+//Function to select movie title
+
+//Function to manipulate the guesses
+
+//Function to input the correct letters for the word selected
+
+
+       
